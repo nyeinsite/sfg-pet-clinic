a770db1c29bf0ceeae6aa0f39e1b88150300cab5
Nyein Yadanar Tun <nyeinyadanartun2001@gmail.com>
2021-03-15 20:32:50 +0630

    added specialities.

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity{
+    private String descripition;
+
+    public String getDescripition() {
+        return descripition;
+    }
+
+    public void setDescripition(String descripition) {
+        this.descripition = descripition;
+    }
+}
