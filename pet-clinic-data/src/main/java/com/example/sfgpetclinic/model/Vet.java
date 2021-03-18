948242e3cbb6ca3baf83204b81c4b8903668ffcb
Nyein Yadanar Tun <nyeinyadanartun2001@gmail.com>
2021-03-18 23:01:42 +0630

    updated vet to use JPA.

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
@@ -1,11 +1,16 @@
 package com.example.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 private Set<Speciality> specialities=new HashSet<>();
-
+    @ManyToMany(fetch = FetchType.EAGER)
+@JoinTable(name = "vet_specialities",
+joinColumns = @JoinColumn(name = "vet_id"),
+inverseJoinColumns = @JoinColumn(name="speciality_id"))
     public Set<Speciality> getSpecialities() {
         return specialities;
     }
