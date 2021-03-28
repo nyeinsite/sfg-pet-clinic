598c19ecfc845090b9542147ced05956f97adecf
Nyein Yadanar Tun <nyeinyadanartun2001@gmail.com>
2021-03-28 20:51:08 +0630

    added create and update owner.

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/BaseEntity.java
@@ -19,5 +19,7 @@ public class BaseEntity implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+public boolean isNew(){
+  return this.id==null;
+}
 }
--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
@@ -2,23 +2,25 @@ package com.example.sfgpetclinic.controllers;
 
 import com.example.sfgpetclinic.model.Owner;
 import com.example.sfgpetclinic.services.OwnerService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
+import javax.jws.WebParam;
 import java.util.List;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
     private OwnerService  ownerService;
+    private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM="owners/createOrUpdateOwnerForm";
 @Autowired
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
@@ -47,7 +49,7 @@ public void setAllowedFields(WebDataBinder dataBinder)
     if(owner.getLastName()==null){
         owner.setLastName("");
     }
-        List<Owner>results=ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner>results=ownerService.findAllByLastNameLike("%"+owner.getLastName()+"%");
     if(results.isEmpty()){
         result.rejectValue("lastName","notFound","not found");
         return "owners/findOwners";
@@ -66,5 +68,34 @@ public void setAllowedFields(WebDataBinder dataBinder)
     mv.addObject(ownerService.findById(ownerId));
     return mv;
     }
+ @GetMapping("/new")
+    public String initCreationForm(Model model){
+        model.addAttribute("owner",Owner.builder().build());
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+ }
 
+ @PostMapping("/new")
+    public String processCreationForm(@Validated Owner owner,BindingResult result){
+        if(result.hasErrors()){
+            return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        }else{
+            Owner savedOwner=ownerService.save(owner);
+            return "redirect:/owners/"+savedOwner.getId();
+        }
+ }
+ @GetMapping("/{ownerId}/edit")
+    public String initUpdateOwnerForm(@PathVariable Long ownerId, Model model){
+        model.addAttribute(ownerService.findById(ownerId));
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+ }
+ @PostMapping("/{ownerId}/edit")
+    public String processUpdateOwnerForm(@Validated Owner owner,BindingResult result,@PathVariable Long ownerId){
+        if(result.hasErrors()){
+            return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        }else {
+            owner.setId(ownerId);
+            Owner savedOwner=ownerService.save(owner);
+            return "redirect:/owners/"+savedOwner.getId();
+        }
+ }
 }
