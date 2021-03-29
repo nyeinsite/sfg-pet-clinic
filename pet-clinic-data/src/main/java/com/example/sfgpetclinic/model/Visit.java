27d1cee1e578c90e8f0a747e2fa39ac688efde20
Nyein Yadanar Tun <nyeinyadanartun2001@gmail.com>
2021-03-29 21:39:23 +0630

    added vet list view.

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Visit.java
@@ -8,11 +8,19 @@ import java.time.LocalDate;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
+
 @Entity
 @Table(name="visits")
 public class Visit extends BaseEntity{
+    @Builder
+    public Visit(Long id, LocalDate date, String description, Pet pet) {
+        super(id);
+        this.date = date;
+        this.description = description;
+        this.pet = pet;
+    }
+
     @Column(name="date")
     private LocalDate date;
     @Column(name="description")
---
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VisitController.java
@@ -0,0 +1,60 @@
+package com.example.sfgpetclinic.controllers;
+
+import com.example.sfgpetclinic.model.Pet;
+import com.example.sfgpetclinic.model.Visit;
+import com.example.sfgpetclinic.services.PetService;
+import com.example.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+import java.util.Map;
+
+@Controller
+public class VisitController {
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+        dataBinder.registerCustomEditor(LocalDate.class,new PropertyEditorSupport(){
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(LocalDate.parse(text));
+            }
+        });
+    }
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Map<String, Object> model) {
+        Pet pet = petService.findById(petId);
+        model.put("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String  initNewVisitForm(@PathVariable("petId")Long petId, Map<String,Object> model){
+        return "pets/createOrUpdateVisitForm";
+    }
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Validated Visit visit, BindingResult result){
+
+        if(result.hasErrors()){
+            return "pets/createOrUpdateVisitForm";
+        }else{
+            visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+}
